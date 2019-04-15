510c84259dd0bdfe19549e53306bbda903e837e9
Puneet Behl <behl4485@gmail.com>
2019-04-15 19:50:30 +0530

    Update docs example for kotlin to use `@ApiResponses`

--- docs-examples/example-kotlin/src/test/kotlin/io/micronaut/configuration/openapi/docs/controllers/annotations/HelloController.kt
+++ docs-examples/example-kotlin/src/test/kotlin/io/micronaut/configuration/openapi/docs/controllers/annotations/HelloController.kt
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 
 // end::imports[]
@@ -23,14 +24,14 @@ open class HelloController {
      * @return The greeting message
      */
     @Get(uri = "/greetings/{name}", produces = [MediaType.TEXT_PLAIN])
-    // Please Note: Repeatable Annotations with non-SOURCE retentions are not yet supported with Kotlin so we are using `responses`
-    // in `@Operations`, instead of `@ApiResponse`, see https://youtrack.jetbrains.com/issue/KT-12794
-    @Operation(summary = "Greets a person", description = "A friendly greeting is returned",
-            responses = [
-                ApiResponse(content = [Content(mediaType = "text/plain", schema = Schema(type = "string"))]),
-                ApiResponse(responseCode = "400", description = "Invalid Name Supplied"),
-                ApiResponse(responseCode = "404", description = "Person not found")
-            ])
+    @Operation(summary = "Greets a person", description = "A friendly greeting is returned")
+    // Please Note: Repeatable Annotations with non-SOURCE retentions are not yet supported with Kotlin so we are using `@ApiResponses`
+    // instead of `@ApiResponse`, see https://youtrack.jetbrains.com/issue/KT-12794
+    @ApiResponses(
+            ApiResponse(content = [Content(mediaType = "text/plain", schema = Schema(type = "string"))]),
+            ApiResponse(responseCode = "400", description = "Invalid Name Supplied"),
+            ApiResponse(responseCode = "404", description = "Person not found")
+    )
     @Tag(name = "greeting")
     open fun greetings(name: String): Single<String> {
         return Single.just("Hello $name, how are you doing?")
