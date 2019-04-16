ac6fc903f1c6161812870fc72882596a34d7c36b
Puneet Behl <behl4485@gmail.com>
2019-04-16 18:40:22 +0530

    Updated travis-build.sh
    * Removed jdk11
    * Publish docs for 1.1.x

--- travis-build.sh
+++ travis-build.sh
@@ -2,27 +2,6 @@
 set -e
 EXIT_STATUS=0
 
-if [ "${TRAVIS_JDK_VERSION}" == "openjdk11" ] ; then
-    echo "Check for branch $TRAVIS_BRANCH JDK: $TRAVIS_JDK_VERSION"
-    ./gradlew testClasses --no-daemon || EXIT_STATUS=$?
-
-    if [ $EXIT_STATUS -ne 0 ]; then
-       exit $EXIT_STATUS
-    fi
-
-    ./gradlew --stop
-    ./gradlew check --no-daemon || EXIT_STATUS=$?
-
-    if [ $EXIT_STATUS -ne 0 ]; then
-       exit $EXIT_STATUS
-    fi
-
-    ./gradlew --stop
-    ./gradlew assemble --no-daemon || EXIT_STATUS=$?
-
-    exit $EXIT_STATUS
-fi
-
 git config --global user.name "$GIT_NAME"
 git config --global user.email "$GIT_EMAIL"
 git config --global credential.helper "store --file=~/.git-credentials"
@@ -37,21 +16,19 @@ if [[ $EXIT_STATUS -eq 0 ]]; then
         ./gradlew testClasses --no-daemon || EXIT_STATUS=$?
 
         ./gradlew --stop
+        killall -9 java
         ./gradlew check --no-daemon || EXIT_STATUS=$?
     fi
 fi
 
 if [[ $EXIT_STATUS -eq 0 ]]; then
     echo "Publishing archives for branch $TRAVIS_BRANCH"
-    if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH =~ ^master$ && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+    if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH =~ ^master|[12]\..\.x$ && $TRAVIS_PULL_REQUEST == 'false' ]]; then
 
       echo "Publishing archives"
       ./gradlew --stop
       if [[ -n $TRAVIS_TAG ]]; then
           ./gradlew bintrayUpload --no-daemon --stacktrace || EXIT_STATUS=$?
-#          if [[ $EXIT_STATUS -eq 0 ]]; then
-#            ./gradlew synchronizeWithMavenCentral --no-daemon
-#          fi
       else
           ./gradlew publish --no-daemon --stacktrace || EXIT_STATUS=$?
       fi
@@ -67,7 +44,6 @@ if [[ $EXIT_STATUS -eq 0 ]]; then
         if [[ $TRAVIS_BRANCH =~ ^master|[12]\..\.x$ ]]; then
            mkdir -p snapshot
            cp -r ../build/docs/. ./snapshot/
-
            git add snapshot/*
         fi
 
