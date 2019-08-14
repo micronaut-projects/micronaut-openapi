d96b4a47eebf1b807b162a1fc1a9bbec43cddd1c
Jacob Aae Mikkelsen <github@grydeske.dk>
2019-08-14 12:09:16 +0200

    Return value should be allowed to be multiline (#51)
    Previously it switched to TEXT state on newline which would add remainder to description.

--- openapi/src/test/groovy/io/micronaut/openapi/javadoc/JavadocParserSpec.groovy
+++ openapi/src/test/groovy/io/micronaut/openapi/javadoc/JavadocParserSpec.groovy
@@ -68,6 +68,41 @@ And more stuff.
 And more stuff.'''
         desc.returnDescription == 'The return value'
         desc.parameters['foo'] == 'The foo param'
+    }
+
+    void 'test parse multiline return value javadoc'() {
+
+        given:
+        JavadocParser parser = new JavadocParser()
+        JavadocDescription desc = parser.parse('''
+<p>This is a description with <b>bold</b> and {@code some code}.</p>
+
+And more stuff.
+
+@since 1.0
+@param foo The foo param
+With more foo param desctiption
+@param bar The {@code bar} param
+@return The {@value return} value
+with more return description
+
+as it is multiline
+''')
+
+
+        expect:
+        desc.methodDescription
+        desc.returnDescription
+        desc.parameters.size() == 2
+        desc.parameters['foo'] == '''The foo param
+With more foo param desctiption'''
+        desc.methodDescription == '''This is a description with bold and some code.
+
+And more stuff.'''
+        desc.returnDescription == '''The return value
+with more return description
+
+as it is multiline'''
 
     }
 }
