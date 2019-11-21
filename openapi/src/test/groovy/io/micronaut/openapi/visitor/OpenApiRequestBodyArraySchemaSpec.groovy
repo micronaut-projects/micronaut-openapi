micronaut-projects/micronaut-openapi

commit 6cd4cee30e112d01c10cc7570ff4eec3cce24161

Allow custom RequestBody with inheritance Schema (#85)

openapi/src/main/java/io/micronaut/openapi/visitor/AbstractOpenApiVisitor.java
@@ -50,6 +50,7 @@ import io.swagger.v3.oas.annotations.links.Link;
 import io.swagger.v3.oas.annotations.links.LinkParameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.DiscriminatorMapping;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.OAuthScope;
 import io.swagger.v3.oas.annotations.servers.ServerVariable;
@@ -161,7 +162,7 @@ abstract class AbstractOpenApiVisitor  {
      * @param clazz The output class instance
      * @param <T> The output class type
      * @return The converted instance
-     * @throws com.fasterxml.jackson.core.JsonProcessingException
+     * @throws com.fasterxml.jackson.core.JsonProcessingException if error
      */
     protected <T> T treeToValue(JsonNode jn, Class<T> clazz) throws com.fasterxml.jackson.core.JsonProcessingException {
         T value = jsonMapper.treeToValue(jn, clazz);
@@ -714,46 +715,9 @@ abstract class AbstractOpenApiVisitor  {
             schema = schemas.get(schemaName);
             if (schema == null) {
                 inProgressSchemas.add(schemaName);
-                Map<CharSequence, Object> values = schemaValue.getValues()
-                        .entrySet()
-                        .stream()
-                        .collect(toMap(e -> e.getKey().equals("requiredProperties") ? "required" : e.getKey(), Map.Entry::getValue));
-                JsonNode schemaJson = toJson(values, context);
                 try {
-                    schema = jsonMapper.treeToValue(schemaJson, Schema.class);
-
+                    schema = readSchema(schemaValue, openAPI, context, mediaType, type);
                     if (schema != null) {
-                        if (schema instanceof ComposedSchema) {
-                            final Optional<String[]> allOf = schemaValue.get("allOf", String[].class);
-                            if (allOf.isPresent() && allOf.get().length > 0) {
-                                final String[] names = allOf.get();
-                                List<Schema> schemaList = namesToSchemas(mediaType, openAPI, context, names);
-
-                                ((ComposedSchema) schema).allOf(schemaList);
-                            }
-
-                            final Optional<String[]> anyOf = schemaValue.get("anyOf", String[].class);
-                            if (anyOf.isPresent() && anyOf.get().length > 0) {
-                                final String[] names = anyOf.get();
-                                List<Schema> schemaList = namesToSchemas(mediaType, openAPI, context, names);
-                                ((ComposedSchema) schema).anyOf(schemaList);
-                            }
-
-                            final Optional<String[]> oneof = schemaValue.get("oneOf", String[].class);
-                            if (oneof.isPresent() && oneof.get().length > 0) {
-                                final String[] names = oneof.get();
-                                List<Schema> schemaList = namesToSchemas(mediaType, openAPI, context, names);
-                                ((ComposedSchema) schema).oneOf(schemaList);
-                            }
-
-                            schema.setType("object");
-                        }
-                        if (type instanceof EnumElement) {
-                            schema.setType("string");
-                            schema.setEnum(((EnumElement) type).values());
-                        } else if (schema instanceof ObjectSchema || schema instanceof ComposedSchema) {
-                            populateSchemaProperties(mediaType, openAPI, context, type, schema);
-                        }
                         schema.setName(schemaName);
                         schemas.put(schemaName, schema);
                     }
@@ -795,10 +759,12 @@ abstract class AbstractOpenApiVisitor  {
                                 schema = new ComposedSchema();
                                 while (superType.isPresent()) {
                                     final ClassElement superElement = superType.get();
-
-                                    final Schema parentSchema = getSchemaDefinition(mediaType, openAPI, context, superElement, null);
-                                    if (parentSchema != null) {
-                                        ((ComposedSchema) schema).allOf(Collections.singletonList(parentSchema));
+                                    String parentSchemaName = computeDefaultSchemaName(definingElement, superElement);
+                                    if (schemas.get(parentSchemaName) != null
+                                            || getSchemaDefinition(mediaType, openAPI, context, superElement, null) != null) {
+                                        Schema parentSchemaRef = new Schema();
+                                        parentSchemaRef.set$ref(schemaRef(parentSchemaName));
+                                        ((ComposedSchema) schema).allOf(Collections.singletonList(parentSchemaRef));
                                     }
                                     superType = superElement.getSuperType();
                                 }
@@ -817,10 +783,82 @@ abstract class AbstractOpenApiVisitor  {
         }
         if (schema != null) {
             Schema schemaRef = new Schema();
-            schemaRef.set$ref("#/components/schemas/" + schema.getName());
+            schemaRef.set$ref(schemaRef(schema.getName()));
             return schemaRef;
         }
         return null;
+    }
+
+    /**
+     * Reads schema.
+     * @param schemaValue annotation value
+     * @param openAPI The OpenApi
+     * @param context The VisitorContext
+     * @param mediaType The media type of schema
+     * @param type The element
+     * @return New schema instance
+     * @throws JsonProcessingException when Json parsing fails
+     */
+    protected Schema readSchema(AnnotationValue<io.swagger.v3.oas.annotations.media.Schema> schemaValue, OpenAPI openAPI, VisitorContext context, @Nullable String mediaType,  @Nullable Element type) throws JsonProcessingException {
+        Map<CharSequence, Object> values = schemaValue.getValues()
+                .entrySet()
+                .stream()
+                .collect(toMap(e -> e.getKey().equals("requiredProperties") ? "required" : e.getKey(), Map.Entry::getValue));
+        JsonNode schemaJson = toJson(values, context);
+        Schema schema = treeToValue(schemaJson, Schema.class);
+        if (schema == null) {
+            return null;
+        }
+        if (schema instanceof ComposedSchema) {
+            final Optional<String[]> allOf = schemaValue.get("allOf", String[].class);
+            if (allOf.isPresent() && allOf.get().length > 0) {
+                final String[] names = allOf.get();
+                List<Schema> schemaList = namesToSchemas(mediaType, openAPI, context, names);
+
+                ((ComposedSchema) schema).allOf(schemaList);
+            }
+
+            final Optional<String[]> anyOf = schemaValue.get("anyOf", String[].class);
+            if (anyOf.isPresent() && anyOf.get().length > 0) {
+                final String[] names = anyOf.get();
+                List<Schema> schemaList = namesToSchemas(mediaType, openAPI, context, names);
+                ((ComposedSchema) schema).anyOf(schemaList);
+            }
+
+            final Optional<String[]> oneof = schemaValue.get("oneOf", String[].class);
+            if (oneof.isPresent() && oneof.get().length > 0) {
+                final String[] names = oneof.get();
+                List<Schema> schemaList = namesToSchemas(mediaType, openAPI, context, names);
+                ((ComposedSchema) schema).oneOf(schemaList);
+            }
+
+            schema.setType("object");
+        }
+        if (type instanceof EnumElement) {
+            schema.setType("string");
+            schema.setEnum(((EnumElement) type).values());
+        } else if (schema instanceof ObjectSchema || schema instanceof ComposedSchema) {
+            populateSchemaProperties(mediaType, openAPI, context, type, schema);
+        }
+        String discriminatorProperty = schemaValue.get("discriminatorProperty", String.class, null);
+        if (StringUtils.isNotEmpty(discriminatorProperty)) {
+            Discriminator discriminator = new Discriminator();
+            discriminator.setPropertyName(discriminatorProperty);
+            Map<String, String> mapping = new LinkedHashMap<>();
+            for (AnnotationValue<DiscriminatorMapping> dm : schemaValue.getAnnotations("discriminatorMapping", DiscriminatorMapping.class)) {
+                context.getClassElement(dm.getRequiredValue("schema", String.class)).ifPresent(classElement -> {
+
+                    final Schema schemaDefinition = getSchemaDefinition(mediaType, openAPI, context, classElement, null);
+                    if (schemaDefinition != null) {
+                        String discriminatorValue = dm.getRequiredValue("value", String.class);
+                        mapping.put(discriminatorValue, schemaDefinition.get$ref());
+                    }
+                });
+            }
+            discriminator.setMapping(mapping);
+            schema.setDiscriminator(discriminator);
+        }
+        return schema;
     }
 
     private List<Schema> namesToSchemas(@Nullable String mediaType, OpenAPI openAPI, VisitorContext context, String[] names) {
@@ -835,6 +873,10 @@ abstract class AbstractOpenApiVisitor  {
 
                                         return Stream.empty();
                                     }).collect(Collectors.toList());
+    }
+
+    private String schemaRef(String schemaName) {
+        return "#/components/schemas/" + schemaName;
     }
 
     private String computeDefaultSchemaName(Element definingElement, Element type) {

openapi/src/main/java/io/micronaut/openapi/visitor/OpenApiControllerVisitor.java
@@ -141,6 +141,7 @@ public class OpenApiControllerVisitor extends AbstractOpenApiVisitor implements 
                 }
             }).orElse(new io.swagger.v3.oas.models.Operation());
 
+            readRequestBody(element, context, openAPI, swaggerOperation);
 
             readTags(element, swaggerOperation);
 
@@ -470,6 +471,37 @@ public class OpenApiControllerVisitor extends AbstractOpenApiVisitor implements 
                 }
             }
         });
+    }
+
+    private void readRequestBody(MethodElement element, VisitorContext context, OpenAPI openAPI, io.swagger.v3.oas.models.Operation swaggerOperation) {
+        AnnotationValue<io.swagger.v3.oas.annotations.parameters.RequestBody> requestBodyAnnotation
+                = element.findAnnotation(io.swagger.v3.oas.annotations.parameters.RequestBody.class).orElse(null);
+        if (requestBodyAnnotation == null) {
+            return;
+        }
+        RequestBody requestBody;
+        try {
+            JsonNode jsonNode = toJson(requestBodyAnnotation.getValues(), context);
+            requestBody = treeToValue(jsonNode, RequestBody.class);
+        } catch (Exception e) {
+            context.warn("Error reading RequestBody for element [" + element + "]: " + e.getMessage(), element);
+            return;
+        }
+        Content content = new Content();
+        for (AnnotationValue<io.swagger.v3.oas.annotations.media.Content> contentAnnotation : requestBodyAnnotation.getAnnotations("content", io.swagger.v3.oas.annotations.media.Content.class)) {
+            String mediaType = contentAnnotation.get("mediaType", String.class).orElse(MediaType.APPLICATION_JSON);
+            AnnotationValue<io.swagger.v3.oas.annotations.media.Schema> schemaAnnotation = contentAnnotation.getAnnotation("schema", io.swagger.v3.oas.annotations.media.Schema.class).get();
+            io.swagger.v3.oas.models.media.MediaType mt = new io.swagger.v3.oas.models.media.MediaType();
+            try {
+                Schema schema = readSchema(schemaAnnotation, openAPI, context, mediaType, null);
+                mt.schema(schema);
+            } catch (Exception e) {
+                context.warn("Error reading Swagger Parameter for element [" + contentAnnotation + "]: " + e.getMessage(), element);
+            }
+            content.addMediaType(mediaType, mt);
+        }
+        requestBody.setContent(content);
+        swaggerOperation.setRequestBody(requestBody);
     }
 
     private boolean isIgnoredParameterType(ClassElement parameterType) {

openapi/src/test/groovy/io/micronaut/openapi/visitor/OpenApiSchemaInheritanceSpec.groovy
@@ -1,0 +1,126 @@
+/*
+ * Copyright 2017-2019 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.openapi.visitor
+
+import io.micronaut.annotation.processing.test.AbstractTypeElementSpec
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.Operation
+import io.swagger.v3.oas.models.parameters.RequestBody
+
+class OpenApiSchemaInheritanceSpec extends AbstractTypeElementSpec {
+    def setup() {
+        System.setProperty(AbstractOpenApiVisitor.ATTR_TEST_MODE, "true")
+    }
+
+    void "test parse the OpenAPI with response that contains generic types"() {
+        given:
+        buildBeanDefinition('test.MyBean', '''
+
+package test;
+
+import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.parameters.*;
+import io.swagger.v3.oas.annotations.responses.*;
+import io.swagger.v3.oas.annotations.security.*;
+import io.swagger.v3.oas.annotations.media.*;
+import io.swagger.v3.oas.annotations.enums.*;
+import io.swagger.v3.oas.annotations.links.*;
+import io.micronaut.http.annotation.*;
+import java.util.List;
+
+@Controller("/")
+class MyController {
+
+    @Put("/")
+    @Operation(summary = "Update an existing pet")
+        @RequestBody(required = true, content = @Content(
+            schema = @Schema(
+                    oneOf = {
+                            A.class, B.class
+                    },
+                    discriminatorMapping = {
+                            @DiscriminatorMapping(value = "A", schema = A.class),
+                            @DiscriminatorMapping(value = "B", schema = B.class)
+                    },
+                    discriminatorProperty = "type")
+    ))
+    public void updatePet(Base base) {
+    }
+}
+
+abstract class Base {
+    private int money;
+    
+    public void setMoney(int a) {
+        money = a;
+    }
+    
+    public int getMoney() {
+        return money;
+    }
+}
+
+class A extends Base {
+    private int age1;
+    
+    public void setAge1(int a) {
+        age1 = a;
+    }
+    
+    public int getAge1() {
+        return age1;
+    }
+}
+
+class B extends Base {
+    private int age2;
+    
+    public void setAge2(int a) {
+        age2 = a;
+    }
+    
+    public int getAge2() {
+        return age2;
+    }
+}
+
+@javax.inject.Singleton
+class MyBean {}
+''')
+
+        OpenAPI openAPI = AbstractOpenApiVisitor.testReference
+        Operation operation = openAPI.paths?.get("/")?.put
+        def requestBody = operation.requestBody
+        def schema = requestBody.content['application/json'].schema
+        requestBody.required
+        schema.oneOf[0].$ref == '#/components/schemas/A'
+        schema.oneOf[1].$ref == '#/components/schemas/B'
+        schema.type == 'object'
+        schema.discriminator.propertyName == 'type'
+        schema.discriminator.mapping['A'] == '#/components/schemas/A'
+        schema.discriminator.mapping['B'] == '#/components/schemas/B'
+
+        expect:
+        operation
+        operation.responses.size() == 1
+        openAPI.components.schemas['A'].properties['age1'].type == 'integer'
+        openAPI.components.schemas['A'].allOf[0].$ref == '#/components/schemas/Base'
+        openAPI.components.schemas['B'].properties['age2'].type == 'integer'
+        openAPI.components.schemas['B'].allOf[0].$ref == '#/components/schemas/Base'
+        openAPI.components.schemas['Base'].properties['money'].type == 'integer'
+    }
+
+}

commit fd6c1791c7c4e7b6f59e61b37c18475823282109

#93 Regression introduced by the PR #85

openapi/src/test/groovy/io/micronaut/openapi/visitor/OpenApiRequestBodyArraySchemaSpec.groovy
@@ -1,0 +1,91 @@
+/*
+ * Copyright 2017-2019 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.openapi.visitor
+
+import io.micronaut.annotation.processing.test.AbstractTypeElementSpec
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.Operation
+import io.swagger.v3.oas.models.media.ArraySchema
+import io.swagger.v3.oas.models.parameters.RequestBody
+
+class OpenApiRequestBodyArraySchemaSpec extends AbstractTypeElementSpec {
+    def setup() {
+        System.setProperty(AbstractOpenApiVisitor.ATTR_TEST_MODE, "true")
+    }
+
+    void "test parse the OpenAPI with ArraySchema in RequestBody"() {
+
+        when:
+        buildBeanDefinition('test.MyBean', '''
+package test;
+
+import java.util.List;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Put;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Controller("/")
+class MyController {
+
+    @Put("/")
+    @Tag(name = "Tag name", description = "tag description.")
+    @Operation(description = "Operation description.", summary = "Operation summary.")
+    @RequestBody(description = "Body description.",
+            required = true,
+            content = @Content(mediaType = MediaType.APPLICATION_JSON,
+                    array = @ArraySchema(schema = @Schema(implementation = Long.class)
+                    )))
+    public void update(List<Long> l) {
+    }
+}
+
+@javax.inject.Singleton
+class MyBean {}
+''')
+
+        OpenAPI openAPI = AbstractOpenApiVisitor.testReference
+
+        then:
+        openAPI
+        openAPI.paths.size() == 1
+        openAPI.paths.get("/")
+        openAPI.paths.get("/").put
+
+        when:
+        Operation operation = openAPI.paths?.get("/")?.put
+        RequestBody requestBody = operation.requestBody
+
+        then:
+        requestBody.required
+        requestBody.description == "Body description."
+        requestBody.content
+        requestBody.content.size() == 1
+        requestBody.content['application/json'].schema
+        requestBody.content['application/json'].schema instanceof ArraySchema
+
+        expect:
+        operation
+        operation.responses.size() == 1
+    }
+
+}