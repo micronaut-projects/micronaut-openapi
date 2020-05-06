f4b9660306de3621ffa17717945c08c5397360a2
croudet <croudet@gmail.com>
2020-05-06 04:09:43 -0400

    Issue #220 - Check if http method has multiple uris. (#222)
    * Issue #220 - Check if http method has multiple uris.
    
    * Reuse empty array.
    
    * checkstyle
    
    * clean-up

--- openapi/src/main/java/io/micronaut/openapi/visitor/OpenApiControllerVisitor.java
+++ openapi/src/main/java/io/micronaut/openapi/visitor/OpenApiControllerVisitor.java
@@ -94,13 +94,24 @@ public class OpenApiControllerVisitor extends AbstractOpenApiEndpointVisitor<Con
     }
 
     @Override
-    protected UriMatchTemplate uriMatchTemplate(MethodElement element) {
+    protected List<UriMatchTemplate> uriMatchTemplates(MethodElement element) {
         String controllerValue = element.getOwningType().getValue(UriMapping.class, String.class).orElse(element.getDeclaringType().getValue(UriMapping.class, String.class).orElse("/"));
         controllerValue = getPropertyPlaceholderResolver().resolvePlaceholders(controllerValue).orElse(controllerValue);
         UriMatchTemplate matchTemplate = UriMatchTemplate.of(controllerValue);
-        String methodValue = element.getValue(HttpMethodMapping.class, String.class).orElse("/");
-        methodValue = getPropertyPlaceholderResolver().resolvePlaceholders(methodValue).orElse(methodValue);
-        return matchTemplate.nest(methodValue);
+        // check if we have multiple uris
+        String[] uris = element.stringValues(HttpMethodMapping.class, "uris");
+        if (uris.length != 0) {
+            List<UriMatchTemplate> matchTemplates = new ArrayList<>(uris.length);
+            for (String methodValue: uris) {
+                methodValue = getPropertyPlaceholderResolver().resolvePlaceholders(methodValue).orElse(methodValue);
+                matchTemplates.add(matchTemplate.nest(methodValue));
+            }
+            return matchTemplates;
+        } else {
+            String methodValue = element.getValue(HttpMethodMapping.class, String.class).orElse("/");
+            methodValue = getPropertyPlaceholderResolver().resolvePlaceholders(methodValue).orElse(methodValue);
+            return Collections.singletonList(matchTemplate.nest(methodValue));
+        }
     }
 
     @Override
