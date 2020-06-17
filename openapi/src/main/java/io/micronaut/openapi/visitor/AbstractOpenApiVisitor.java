252195ccd2a1e1758e95d2284cca19427c99afd1
croudet <croudet@gmail.com>
2020-06-17 11:22:30 -0400

    Add null check on class element.

--- openapi/src/main/java/io/micronaut/openapi/visitor/AbstractOpenApiVisitor.java
+++ openapi/src/main/java/io/micronaut/openapi/visitor/AbstractOpenApiVisitor.java
@@ -1217,8 +1217,8 @@ abstract class AbstractOpenApiVisitor  {
      * @return true if classElement is a JavaClassElement.
      */
     static boolean isJavaElement(ClassElement classElement, VisitorContext context) {
-        return ("io.micronaut.annotation.processing.visitor.JavaClassElement".equals(classElement.getClass().getName())
-                || "io.micronaut.annotation.processing.visitor.JavaClassElementExt".equals(classElement.getClass().getName())) && "io.micronaut.annotation.processing.visitor.JavaVisitorContext".equals(context.getClass().getName());
+        return classElement != null && (("io.micronaut.annotation.processing.visitor.JavaClassElement".equals(classElement.getClass().getName())
+                || "io.micronaut.annotation.processing.visitor.JavaClassElementExt".equals(classElement.getClass().getName())) && "io.micronaut.annotation.processing.visitor.JavaVisitorContext".equals(context.getClass().getName()));
     }
 
     private void populateSchemaProperties(OpenAPI openAPI, VisitorContext context, Element type, Schema schema, List<MediaType> mediaTypes) {
