8c01ab6f6bf62df70ac2413761e740cf31731322
cagataysel <selcagatay1@hotmail.com>
2021-09-29 17:54:32 +0300

    Add a schema creation test for interfaces with inheritance

--- openapi/src/test/groovy/io/micronaut/openapi/visitor/OpenApiInheritedPojoControllerSpec.groovy
+++ openapi/src/test/groovy/io/micronaut/openapi/visitor/OpenApiInheritedPojoControllerSpec.groovy
@@ -1054,4 +1054,87 @@ class MyBean {}
         ((ComposedSchema) operation.responses."200".content."application/json".schema).oneOf[1].get$ref() == "#/components/schemas/Cat"
     }
 
+  void "test build OpenAPI doc for interface POJO with inheritance"() {
+
+    given: "An API definition"
+    when:
+    buildBeanDefinition('test.MyBean', '''
+package test;
+
+import io.reactivex.*;
+import io.micronaut.http.annotation.*;
+import com.fasterxml.jackson.annotation.*;
+import java.util.List;
+import io.swagger.v3.oas.annotations.media.*;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.micronaut.http.MediaType;
+
+@Controller("/pets")
+interface PetOperations {
+
+    /**
+     * @param name The person's name
+     * @return The greeting
+     */
+    @Get(uri = "/cases/{name}", produces = MediaType.TEXT_PLAIN)
+    Cat getCat(String name);
+
+        /**
+     * @param name The person's name
+     * @return The greeting
+     */
+    @Get(uri = "/dogs/{name}", produces = MediaType.TEXT_PLAIN)
+    Dog getDog(String name);
+}
+
+interface Dog extends Pet {  
+
+    public String getBreed();
+}
+
+interface Cat extends Pet {    
+
+    public int getClawSize();
+}
+
+@JsonTypeInfo(include = JsonTypeInfo.As.PROPERTY, use = JsonTypeInfo.Id.NAME, property = "type", visible = true)
+@JsonSubTypes({ @JsonSubTypes.Type(value = Cat.class, name = "Cat"),
+        @JsonSubTypes.Type(value = Dog.class, name = "Dog") })
+interface Pet {  
+  
+    public int getAge();
+
+    public String getName();
+}
+
+@jakarta.inject.Singleton
+class MyBean {}
+''')
+    then: "the state is correct"
+    AbstractOpenApiVisitor.testReference != null
+
+    when: "The OpenAPI is retrieved"
+    OpenAPI openAPI = AbstractOpenApiVisitor.testReference
+    Schema petSchema = openAPI.components.schemas['Pet']
+    Schema catSchema = openAPI.components.schemas['Cat']
+    Schema dogSchema = openAPI.components.schemas['Dog']
+
+    then: "the components are valid"
+    petSchema != null
+    dogSchema != null
+    catSchema != null
+    petSchema instanceof ComposedSchema
+    catSchema instanceof ComposedSchema
+    dogSchema instanceof ComposedSchema
+    catSchema.type == null
+    catSchema.properties == null
+    petSchema.type == 'object'
+    petSchema.properties.size() == 2
+
+    ((ComposedSchema)catSchema).allOf.size() == 2
+    ((ComposedSchema)catSchema).allOf[0].$ref == '#/components/schemas/Pet'
+    ((ComposedSchema)catSchema).allOf[1].type == 'object'
+    ((ComposedSchema)catSchema).allOf[1].properties['clawSize'].type == 'integer'
+  }
+
 }
