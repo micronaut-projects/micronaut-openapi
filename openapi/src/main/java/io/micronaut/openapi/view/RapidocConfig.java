c6c7b379aaed2d77fae903b2f166f05ef387b318
Ed Mitchell <edeesis@gmail.com>
2022-10-23 14:18:46 -0500

    fix #829 Fix issue with javascript path rendering in views

--- openapi/src/main/java/io/micronaut/openapi/view/RapidocConfig.java
+++ openapi/src/main/java/io/micronaut/openapi/view/RapidocConfig.java
@@ -398,7 +398,7 @@ final class RapidocConfig extends AbstractViewConfig implements Renderer {
     @Override
     public String render(String template, VisitorContext context) {
         template = rapiPDFConfig.render(template, RendererType.RAPIDOC, context);
-        template = OpenApiViewConfig.replacePlaceHolder(template, "rapidoc.js.url", getFinalUrl(context), "");
+        template = OpenApiViewConfig.replacePlaceHolder(template, "rapidoc.js.url", OpenApiViewConfig.TEMPLATES_RAPIDOC + OpenApiViewConfig.SLASH + getFinalUrl(context), "");
         return OpenApiViewConfig.replacePlaceHolder(template, "rapidoc.attributes", toHtmlAttributes(), "");
     }
 
--- openapi/src/main/java/io/micronaut/openapi/view/RedocConfig.java
+++ openapi/src/main/java/io/micronaut/openapi/view/RedocConfig.java
@@ -147,7 +147,7 @@ final class RedocConfig extends AbstractViewConfig implements Renderer {
     @Override
     public String render(String template, VisitorContext context) {
         template = rapiPDFConfig.render(template, RendererType.REDOC, context);
-        template = OpenApiViewConfig.replacePlaceHolder(template, "redoc.js.url", getFinalUrl(context), "");
+        template = OpenApiViewConfig.replacePlaceHolder(template, "redoc.js.url", OpenApiViewConfig.TEMPLATES_REDOC + OpenApiViewConfig.SLASH + getFinalUrl(context), "");
         return OpenApiViewConfig.replacePlaceHolder(template, "redoc.attributes", toHtmlAttributes(), "");
     }
 
