2c62e86818b90fcd8418a52fa8723bf32b823603
altro3 <alexuyt@gmail.com>
2022-11-12 21:57:45 +0700

    Add ability to customize classes schemas in properties.
    Fixed #654

---
+++ openapi/src/test/groovy/io/micronaut/openapi/MyJaxbElement2.java
@@ -0,0 +1,25 @@
+package io.micronaut.openapi;
+
+import java.util.List;
+
+class MyJaxbElement2 {
+
+    private String type;
+    private List<String> values;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public List<String> getValues() {
+        return values;
+    }
+
+    public void setValues(List<String> values) {
+        this.values = values;
+    }
+}
