80bcd39ccf9e7f64d115b072cd2a31b31f97be72
kevind-wgu <97059907+kevind-wgu@users.noreply.github.com>
2023-01-03 15:05:40 -0700

    Fix missing import

--- openapi/src/main/java/io/micronaut/openapi/visitor/OpenApiApplicationVisitor.java
+++ openapi/src/main/java/io/micronaut/openapi/visitor/OpenApiApplicationVisitor.java
@@ -37,6 +37,7 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Properties;
+import java.util.Set;
 import java.util.TreeMap;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
