ce94d615d6e4a206ef7fb8fc9bd1ed399c4fa933
altro3 <alexuyt@gmail.com>
2023-07-16 13:38:27 +0700

    Add process WildcardElement and GenericPlaceholderElement in generics
    Fixed #1131

--- openapi/src/main/java/io/micronaut/openapi/visitor/AbstractOpenApiVisitor.java
+++ openapi/src/main/java/io/micronaut/openapi/visitor/AbstractOpenApiVisitor.java
@@ -78,6 +78,7 @@ import io.micronaut.inject.ast.GenericPlaceholderElement;
 import io.micronaut.inject.ast.MemberElement;
 import io.micronaut.inject.ast.PropertyElement;
 import io.micronaut.inject.ast.TypedElement;
+import io.micronaut.inject.ast.WildcardElement;
 import io.micronaut.inject.visitor.VisitorContext;
 import io.micronaut.openapi.javadoc.JavadocDescription;
 import io.micronaut.openapi.swagger.core.util.PrimitiveType;
@@ -769,14 +770,27 @@ abstract class AbstractOpenApiVisitor {
             }
         }
 
+        Boolean isArray = null;
+        Boolean isIterable = null;
+
         ClassElement componentType = type != null ? type.getFirstTypeArgument().orElse(null) : null;
+        if (type instanceof WildcardElement wildcardEl) {
+            type = CollectionUtils.isNotEmpty(wildcardEl.getUpperBounds()) ? wildcardEl.getUpperBounds().get(0) : null;
+        } else if (type instanceof GenericPlaceholderElement placeholderEl) {
+            isArray = type.isArray();
+            isIterable = type.isIterable();
+            type = placeholderEl.getResolved().orElse(CollectionUtils.isNotEmpty(placeholderEl.getBounds()) ? placeholderEl.getBounds().get(0) : null);
+        }
         Map<String, ClassElement> typeArgs = type != null ? type.getTypeArguments() : null;
 
         Schema<?> schema = null;
 
         if (type instanceof EnumElement enumEl) {
             schema = getSchemaDefinition(openAPI, context, enumEl, typeArgs, definingElement, mediaTypes);
-        } else {
+            if (isArray != null && isArray) {
+                schema = SchemaUtils.arraySchema(schema);
+            }
+        } else if (type != null) {
 
             boolean isPublisher = false;
             boolean isObservable = false;
@@ -812,6 +826,13 @@ abstract class AbstractOpenApiVisitor {
 
             if (type != null) {
 
+                if (isArray == null) {
+                    isArray = type.isArray();
+                }
+                if (isIterable == null) {
+                    isIterable = type.isIterable();
+                }
+
                 String typeName = type.getName();
                 ClassElement customTypeSchema = OpenApiApplicationVisitor.getCustomSchema(typeName, typeArgs, context);
                 if (customTypeSchema != null) {
@@ -825,13 +846,13 @@ abstract class AbstractOpenApiVisitor {
                     typeName = PrimitiveType.BINARY.name();
                 }
                 PrimitiveType primitiveType = PrimitiveType.fromName(typeName);
-                if (!type.isArray() && ClassUtils.isJavaLangType(typeName)) {
+                if (!isArray && ClassUtils.isJavaLangType(typeName)) {
                     schema = getPrimitiveType(typeName);
-                } else if (!type.isArray() && primitiveType != null) {
+                } else if (!isArray && primitiveType != null) {
                     schema = primitiveType.createProperty();
                 } else if (type.isAssignable(Map.class.getName())) {
                     schema = new MapSchema();
-                    if (typeArgs.isEmpty()) {
+                    if (CollectionUtils.isEmpty(typeArgs)) {
                         schema.setAdditionalProperties(true);
                     } else {
                         ClassElement valueType = typeArgs.get("V");
@@ -841,8 +862,8 @@ abstract class AbstractOpenApiVisitor {
                             schema.setAdditionalProperties(resolveSchema(openAPI, type, valueType, context, mediaTypes, null, classJavadoc));
                         }
                     }
-                } else if (type.isIterable()) {
-                    if (type.isArray()) {
+                } else if (isIterable) {
+                    if (isArray) {
                         schema = resolveSchema(openAPI, type, type.fromArray(), context, mediaTypes, null, classJavadoc);
                         if (schema != null) {
                             schema = SchemaUtils.arraySchema(schema);
