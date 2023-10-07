3faf826622e5eb8e141fadff9e82c1e6faf308a0
altro3 <alexuyt@gmail.com>
2023-10-07 18:26:40 +0700

    Added generating generic annotations to generator.
    Fixes #1239

--- openapi-generator/src/main/java/io/micronaut/openapi/generator/AbstractMicronautJavaCodegen.java
+++ openapi-generator/src/main/java/io/micronaut/openapi/generator/AbstractMicronautJavaCodegen.java
@@ -231,10 +231,12 @@ public abstract class AbstractMicronautJavaCodegen<T extends GeneratorOptionsBui
         };
         reservedWords.addAll(Arrays.asList(reservedWordsArray));
 
+        importMapping.put("DateTime", "java.time.Instant");
         importMapping.put("LocalDateTime", "java.time.LocalDateTime");
         importMapping.put("OffsetDateTime", "java.time.OffsetDateTime");
         importMapping.put("ZonedDateTime", "java.time.ZonedDateTime");
         importMapping.put("LocalDate", "java.time.LocalDate");
+        importMapping.put("LocalTime", "java.time.LocalTime");
     }
 
     public void setGenerateHttpResponseAlways(boolean generateHttpResponseAlways) {
@@ -658,6 +660,10 @@ public abstract class AbstractMicronautJavaCodegen<T extends GeneratorOptionsBui
                 });
                 op.formParams.clear();
             }
+
+            for (var param : op.allParams) {
+                processGenericAnnotations(param.dataType, param.datatypeWithEnum, param.isArray, param.items, param.vendorExtensions);
+            }
         }
 
         return objs;
@@ -887,16 +893,176 @@ public abstract class AbstractMicronautJavaCodegen<T extends GeneratorOptionsBui
                 property.vendorExtensions.put("lombok", lombok);
                 property.vendorExtensions.put("defaultValueIsNotNull", property.defaultValue != null && !property.defaultValue.equals("null"));
                 property.vendorExtensions.put("isServer", isServer);
+                processGenericAnnotations(property.dataType, property.datatypeWithEnum, property.isArray, property.items, property.vendorExtensions);
             }
             model.vendorExtensions.put("isServer", isServer);
             for (var property : model.requiredVars) {
                 property.vendorExtensions.put("lombok", lombok);
                 property.vendorExtensions.put("isServer", isServer);
                 property.vendorExtensions.put("defaultValueIsNotNull", property.defaultValue != null && !property.defaultValue.equals("null"));
+                processGenericAnnotations(property.dataType, property.datatypeWithEnum, property.isArray, property.items, property.vendorExtensions);
             }
         }
 
         return objs;
+    }
+
+    private void processGenericAnnotations(String dataType, String dataTypeWithEnum, boolean isArray, CodegenProperty itemsProp, Map<String, Object> ext) {
+        var typeWithGenericAnnotations = dataType;
+        var typeWithEnumWithGenericAnnotations = dataTypeWithEnum;
+        var addedGenericAnnotations = false;
+        if (useBeanValidation && isArray && itemsProp != null && isPrimitive(itemsProp.openApiType) && dataType.contains("<")) {
+            var genericAnnotations = genericAnnotations(itemsProp);
+            if (itemsProp.isArray) {
+                processGenericAnnotations(itemsProp.dataType, itemsProp.datatypeWithEnum, true, itemsProp.items, itemsProp.vendorExtensions);
+                typeWithGenericAnnotations = addGenericAnnotations((String) itemsProp.vendorExtensions.get("typeWithGenericAnnotations"), dataType, genericAnnotations);
+                typeWithEnumWithGenericAnnotations = addGenericAnnotations((String) itemsProp.vendorExtensions.get("typeWithEnumWithGenericAnnotations"), dataTypeWithEnum, genericAnnotations);
+            } else {
+                typeWithGenericAnnotations = addGenericAnnotations(dataType, null, genericAnnotations);
+                typeWithEnumWithGenericAnnotations = addGenericAnnotations(dataTypeWithEnum, null, genericAnnotations);
+            }
+        }
+        ext.put("typeWithGenericAnnotations", typeWithGenericAnnotations);
+        ext.put("typeWithEnumWithGenericAnnotations", typeWithEnumWithGenericAnnotations);
+    }
+
+    private String addGenericAnnotations(String type, String wrapType, String genericAnnotations) {
+        if (StringUtils.isEmpty(type) || StringUtils.isEmpty(genericAnnotations)) {
+            return type;
+        }
+        var t = wrapType != null ? wrapType : type;
+        var diamondOpen = t.indexOf('<');
+        var diamondClose = t.lastIndexOf('>');
+        var containerType = t.substring(0, diamondOpen);
+        var elementType = t.substring(diamondOpen + 1, diamondClose);
+        return containerType + '<' + genericAnnotations + (wrapType != null ? type : elementType) + '>';
+    }
+
+    private boolean isPrimitive(String type) {
+        return switch (type.toLowerCase()) {
+            case "array", "string", "boolean", "byte", "uri", "url", "uuid", "email", "integer", "long", "float", "double",
+                "number", "partial-time", "date", "date-time", "bigdecimal", "biginteger" -> true;
+            default -> false;
+        };
+    }
+
+    private String genericAnnotations(CodegenProperty prop) {
+
+        var type = prop.openApiType.toLowerCase();
+
+        var result = new StringBuilder();
+        if (StringUtils.isNotEmpty(prop.pattern)) {
+            if (type.equals("email")) {
+                result.append("@Email(regexp = \"");
+            } else {
+                result.append("@Pattern(regexp = \"");
+            }
+            result.append(prop.pattern).append("\") ");
+        }
+
+        var containsNotEmpty = false;
+
+        if (prop.minLength != null || prop.maxLength != null) {
+            if (prop.minLength != null && prop.minLength == 1 && prop.maxLength == null && !prop.isNullable) {
+                result.append("@NotEmpty ");
+                containsNotEmpty = true;
+            } else {
+                result.append("@Size(");
+                if (prop.minLength != null) {
+                    result.append("min = ").append(prop.minLength);
+                }
+                if (prop.maxLength != null) {
+                    if (prop.minLength != null) {
+                        result.append(", ");
+                    }
+                    result.append("max = ").append(prop.maxLength);
+                }
+                result.append(") ");
+            }
+        }
+
+        if (prop.minItems != null || prop.maxItems != null) {
+            if (prop.minItems != null && prop.minItems == 1 && prop.maxItems == null && !prop.isNullable) {
+                result.append("@NotEmpty ");
+                containsNotEmpty = true;
+            } else {
+                result.append("@Size(");
+                if (prop.minItems != null) {
+                    result.append("min = ").append(prop.minItems);
+                }
+                if (prop.maxItems != null) {
+                    if (prop.minItems != null) {
+                        result.append(", ");
+                    }
+                    result.append("max = ").append(prop.maxItems);
+                }
+                result.append(") ");
+            }
+        }
+        if (prop.isNullable) {
+            if (isGenerateHardNullable()) {
+                result.append("@HardNullable ");
+            } else {
+                result.append("@Nullable ");
+            }
+        } else if (!containsNotEmpty) {
+            result.append("@NotNull ");
+        }
+        if (StringUtils.isNotEmpty(prop.minimum)) {
+            try {
+                var longNumber = Long.parseLong(prop.minimum);
+                if (prop.exclusiveMinimum) {
+                    longNumber++;
+                }
+                if (longNumber == 0 && StringUtils.isEmpty(prop.maximum)) {
+                    result.append("@PositiveOrZero ");
+                } else if (longNumber == 1 && StringUtils.isEmpty(prop.maximum)) {
+                    result.append("@Positive ");
+                } else {
+                    result.append("@Min(").append(longNumber).append(") ");
+                }
+            } catch (Exception e) {
+                result.append("@DecimalMin(");
+                if (prop.exclusiveMinimum) {
+                    result.append("value = ");
+                }
+                result.append('"').append(prop.minimum).append('"');
+                if (prop.exclusiveMinimum) {
+                    result.append(", inclusive = false");
+                }
+                result.append(") ");
+            }
+        }
+        if (StringUtils.isNotEmpty(prop.maximum)) {
+            try {
+                var longNumber = Long.parseLong(prop.maximum);
+                if (prop.exclusiveMaximum) {
+                    longNumber--;
+                }
+                if (longNumber == 0 && StringUtils.isEmpty(prop.minimum)) {
+                    result.append("@NegativeOrZero ");
+                } else if (longNumber == -1 && StringUtils.isEmpty(prop.minimum)) {
+                    result.append("@Negative ");
+                } else {
+                    result.append("@Max(").append(longNumber).append(") ");
+                }
+            } catch (Exception e) {
+                result.append("@DecimalMax(");
+                if (prop.exclusiveMaximum) {
+                    result.append("value = ");
+                }
+                result.append('"').append(prop.maximum).append('"');
+                if (prop.exclusiveMaximum) {
+                    result.append(", inclusive = false");
+                }
+                result.append(") ");
+            }
+        }
+        return result.toString();
+    }
+
+    public boolean isGenerateHardNullable() {
+        return false;
     }
 
     private void processParentModel(CodegenModel model, List<CodegenProperty> requiredVarsWithoutDiscriminator, List<CodegenProperty> requiredParentVarsWithoutDiscriminator) {
