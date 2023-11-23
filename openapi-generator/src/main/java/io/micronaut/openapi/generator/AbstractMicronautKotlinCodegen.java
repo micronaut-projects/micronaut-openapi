0f3947d8ff5d2e34a47eef204f3279f56e44b851
altro3 <alexuyt@gmail.com>
2023-11-23 22:04:58 +0700

    Fix generator for properties with second upper case char (#1313)
    Fixed #1226

--- openapi-generator/src/main/java/io/micronaut/openapi/generator/AbstractMicronautKotlinCodegen.java
+++ openapi-generator/src/main/java/io/micronaut/openapi/generator/AbstractMicronautKotlinCodegen.java
@@ -1002,6 +1002,20 @@ public abstract class AbstractMicronautKotlinCodegen<T extends GeneratorOptionsB
     }
 
     @Override
+    public String toVarName(String name) {
+
+        var varName = super.toVarName(name);
+        // Micronaut can't process correctly properties like `eTemperature`, when first symbol in lower case
+        // and second symbol in upper case.
+        // See this: https://github.com/micronaut-projects/micronaut-core/pull/10130
+        if (varName.length() >= 2 && Character.isLowerCase(varName.charAt(0)) && Character.isUpperCase(varName.charAt(1))) {
+            varName = "" + varName.charAt(0) + Character.toLowerCase(varName.charAt(1)) + varName.substring(2);
+        }
+
+        return varName;
+    }
+
+    @Override
     public Map<String, ModelsMap> postProcessAllModels(Map<String, ModelsMap> objs) {
         objs = super.postProcessAllModels(objs);
 
