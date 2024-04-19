0b97099ef333f2e1ab965e14e88963b99eca506d
altro3 <alexuyt@gmail.com>
2024-04-19 14:30:43 +0700

    Micronaut 4.4.4. (#1543)
    Enabled fixed tests

--- test-suite-kotlin-ksp-server-generator/src/test/kotlin/io/micronaut/openapi/test/api/RequestBodyControllerTest.kt
+++ test-suite-kotlin-ksp-server-generator/src/test/kotlin/io/micronaut/openapi/test/api/RequestBodyControllerTest.kt
@@ -21,7 +21,6 @@ import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.Arguments.arguments
 import org.junit.jupiter.params.provider.EnumSource
 import org.junit.jupiter.params.provider.MethodSource
-import spock.lang.Issue
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.time.OffsetDateTime
@@ -198,8 +197,7 @@ class RequestBodyControllerTest(
         assertEquals(model, response)
     }
 
-    @Issue("https://github.com/micronaut-projects/micronaut-serialization/issues/768")
-    @Disabled
+    @Disabled("https://github.com/micronaut-projects/micronaut-serialization/issues/768")
     @MethodSource("discriminators")
     @ParameterizedTest
     fun testSendModelWithDiscriminatorChild(discriminatorName: String, model: Animal) {
