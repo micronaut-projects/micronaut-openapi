1874abe5a9a57b303e4e50b20af11cde47eb1fe4
altro3 <alexuyt@gmail.com>
2024-07-09 14:55:04 +0700

    Add more deep test for "test class property is not exposed"

--- openapi/src/test/groovy/io/micronaut/openapi/visitor/OpenApiClassPropertySpec.groovy
+++ openapi/src/test/groovy/io/micronaut/openapi/visitor/OpenApiClassPropertySpec.groovy
@@ -4,26 +4,21 @@ import io.micronaut.openapi.AbstractOpenApiTypeElementSpec
 import io.swagger.v3.oas.models.OpenAPI
 
 class OpenApiClassPropertySpec extends AbstractOpenApiTypeElementSpec {
+
     void "test class property is not exposed"() {
 
         when:
         buildBeanDefinition("test.MyBean", '''
 package test;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.micronaut.core.annotation.Introspected;
 import io.micronaut.http.HttpResponse;
-import io.micronaut.http.MediaType;
-import io.micronaut.http.annotation.*;
-
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
 import io.swagger.v3.oas.annotations.Hidden;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
-import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.NotBlank;
 
 @Controller
 class PersonController {
@@ -56,6 +51,29 @@ class Person {
     public Class<?> getPersonClass() {
         return personClass;
     }
+
+    public Class<?> getTestProp() {
+        return personClass;
+    }
+
+    public String getNotHiddenProp() {
+        return "";
+    }
+
+    @Hidden
+    public String getHiddenProp2() {
+        return "";
+    }
+
+    @JsonIgnore
+    public String getHiddenProp3() {
+        return "";
+    }
+
+    @Schema(hidden = true)
+    public String getHiddenProp4() {
+        return "";
+    }
 }
 
 @jakarta.inject.Singleton
@@ -70,8 +88,9 @@ public class MyBean {}
         openAPI.components.schemas["Person"].type == "object"
 
         openAPI.components.schemas["Person"].properties
-        openAPI.components.schemas["Person"].properties.size() == 1
+        openAPI.components.schemas["Person"].properties.size() == 2
 
         openAPI.components.schemas["Person"].properties["name"]
+        openAPI.components.schemas["Person"].properties["notHiddenProp"]
     }
 }
