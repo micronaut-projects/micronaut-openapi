47b98360fc0e1ae42bfa1529504c51e8e4d1163f
altro3 <alexuyt@gmail.com>
2024-09-30 20:28:59 +0700

    Fix generating models with polymorphism (#1795)
    Fixed #1794

--- openapi-generator/src/main/java/io/micronaut/openapi/generator/AbstractMicronautKotlinCodegen.java
+++ openapi-generator/src/main/java/io/micronaut/openapi/generator/AbstractMicronautKotlinCodegen.java
@@ -1647,6 +1647,11 @@ public abstract class AbstractMicronautKotlinCodegen<T extends GeneratorOptionsB
             for (var property : model.requiredVars) {
                 processProperty(property, isServer, model, objs);
             }
+            if (model.parentVars != null) {
+                for (var property : model.parentVars) {
+                    processProperty(property, isServer, model, objs);
+                }
+            }
             addStrValueToEnum(model);
         }
 
@@ -1724,6 +1729,7 @@ public abstract class AbstractMicronautKotlinCodegen<T extends GeneratorOptionsB
         property.vendorExtensions.put("isServer", isServer);
         property.vendorExtensions.put("defaultValueIsNotNull", property.defaultValue != null && !property.defaultValue.equals("null"));
         property.vendorExtensions.put("fieldAnnPrefix", ksp ? "" : "field:");
+        property.vendorExtensions.put("x-implements", model.vendorExtensions.get("x-implements"));
         if ("null".equals(property.example)) {
             property.example = null;
         }
