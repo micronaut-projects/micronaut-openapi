13c9728703220e57847ce4c7032a3a23b0701f50
issam liqali <liqali.issam@oracle.com>
2026-02-19 09:42:25 +0000

    update micronaut versions

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,6 +9,5 @@ repositories {
 
 dependencies {
     implementation(libs.micronaut.gradle.plugin)
-    implementation(libs.micronaut.shared.settings)
     implementation(providers.gradleProperty("micronaut-build-version").map { "io.micronaut.build.internal:micronaut-kotlin-build-plugins:${it}" }.get())
 }
