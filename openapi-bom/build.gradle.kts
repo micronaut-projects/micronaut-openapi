ea13c1de19cd5c7770dee0a201c55d06bc93e0b1
Sergio del Amo <sergio.delamo@softamo.com>
2026-03-04 13:19:29 +0100

    remove changes to BOM

--- openapi-bom/build.gradle.kts
+++ openapi-bom/build.gradle.kts
@@ -13,9 +13,6 @@ micronautBom {
 
         acceptedVersionRegressions.add("jakarta-validation-api")
         acceptedLibraryRegressions.add("jakarta-validation-api")
-        dependencies.add("io.swagger.core.v3:swagger-core:2.2.43")
-        dependencies.add("io.swagger.core.v3:swagger-models:2.2.43")
-        dependencies.add("org.jsoup:jsoup:1.22.1")
 
     }
 }
