21ca9c31e6b5f67fcdad79be753fe800ba6979ac
Sergio del Amo <sergio.delamo@softamo.com>
2026-05-18 15:45:36 +0200

    Update Micronaut plugin version to 8.0.0-M18

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,7 @@ dependencyResolutionManagement {
 }
 
 plugins {
-    id("io.micronaut.build.shared.settings") version "8.0.0"
+    id("io.micronaut.build.shared.settings") version "8.0.0-M18"
 }
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
